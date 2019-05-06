bbfd115d4e08db48d38f00a52dfd7b29c1ebc21d
zhangtsh5 <49588117+zhangtsh5@users.noreply.github.com>
2019-05-06 18:37:14 +0800

    Update ps1b.py

--- Problem-set1/ps1b.py
+++ Problem-set1/ps1b.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 17 10:52:02 2019
 
-@author: zhangtieshan
-"""
 
 annual_salary=int(input("Enter your annual salary: "))
 portion_saved=float(input("Enter the percent of your salary to save,as a decimal: "))
