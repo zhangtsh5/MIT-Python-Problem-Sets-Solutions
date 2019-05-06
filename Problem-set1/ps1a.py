9099ce3a879d0e9c96529bbb55257220031bb292
zhangtsh5 <49588117+zhangtsh5@users.noreply.github.com>
2019-05-06 18:36:43 +0800

    Update ps1a.py

--- Problem-set1/ps1a.py
+++ Problem-set1/ps1a.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 16 14:59:25 2019
 
-@author: zhangtieshan
-"""
 annual_salary=int(input("Enter your annual salary: "))
 portion_saved=float(input("Enter the percent of your salary to save,as a decimal: "))
 total_cost=int(input("Enter the cost of your dream home: "))
